517733f5e59828ccbeaff87f1822c793e34d3023
TsarkFC <digasromao2000@gmail.com>
2020-04-16 18:33:10 +0100

    Previous commit missing code

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
 
 public class Game {
+    private Gui gui;
+    public static void main(String[] args) throws IOException, InterruptedException {
+        new Game().start();
+    }
+
+    private void start() throws IOException, InterruptedException {
+        gui = new Gui();
+    }
 }
--- src/main/java/Gui.java
+++ src/main/java/Gui.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class Gui {
+    private TerminalScreen screen;
+
+    public Gui() throws IOException, InterruptedException {
+        try {
+            Terminal terminal = new DefaultTerminalFactory().createTerminal();
+            screen = new TerminalScreen(terminal);
+
+            screen.setCursorPosition(null);   // we don't need a cursor
+            screen.startScreen();             // screens must be started
+            screen.doResizeIfNecessary();     // resize screen if necessary
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        screen.clear();
+        screen.setCharacter(10, 10, new TextCharacter('X'));
+        screen.refresh();
+
+        TimeUnit.SECONDS.sleep(2);
+
+        screen.close();
+    }
 }
