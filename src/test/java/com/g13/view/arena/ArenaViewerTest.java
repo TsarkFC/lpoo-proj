4c0902bf5ce2578fe70bb3c8546418e697ce84c2
TsarkFC <digasromao2000@gmail.com>
2020-05-30 19:41:29 +0100

    Added pbt for input from keyboard

--- src/test/java/com/g13/view/arena/ArenaViewerTest.java
+++ src/test/java/com/g13/view/arena/ArenaViewerTest.java
@@ -1,13 +1,13 @@
 package com.g13.view.arena;
 
 import com.g13.model.arena.Arena;
-import com.g13.view.menus.MenuViewer;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.TerminalScreen;
-import org.junit.jupiter.api.Test;
+import net.jqwik.api.ForAll;
+import net.jqwik.api.Property;
 import org.mockito.Mockito;
 
 import java.io.IOException;
@@ -16,8 +16,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.*;
 
 public class ArenaViewerTest {
-    @Test
-    public void testKeyboardInput() throws IOException {
+    @Property
+    public void testKeyboardInput(@ForAll ArenaViewer.COMMAND command) throws IOException {
         TerminalScreen screen = Mockito.mock(TerminalScreen.class);
         Arena arena = Mockito.mock(Arena.class);
         KeyStroke input = Mockito.mock(KeyStroke.class);
@@ -32,37 +32,40 @@ public class ArenaViewerTest {
 
         Mockito.when(screen.readInput()).thenReturn(input);
 
-        Mockito.when(input.getKeyType()).thenReturn(KeyType.Character);
-        Mockito.when(input.getCharacter()).thenReturn('1');
-        assertEquals(ArenaViewer.COMMAND.ONE, viewer.getNextCommand());
+        if (command == ArenaViewer.COMMAND.ONE) {
+            Mockito.when(input.getKeyType()).thenReturn(KeyType.Character);
+            Mockito.when(input.getCharacter()).thenReturn('1');
+        }
+        else if (command == ArenaViewer.COMMAND.TWO) {
+            Mockito.when(input.getKeyType()).thenReturn(KeyType.Character);
+            Mockito.when(input.getCharacter()).thenReturn('2');
+        }
+        else if (command == ArenaViewer.COMMAND.THREE) {
+            Mockito.when(input.getKeyType()).thenReturn(KeyType.Character);
+            Mockito.when(input.getCharacter()).thenReturn('3');
+        }
+        else if (command == ArenaViewer.COMMAND.FOUR) {
+            Mockito.when(input.getKeyType()).thenReturn(KeyType.Character);
+            Mockito.when(input.getCharacter()).thenReturn('4');
+        }
+        else if (command == ArenaViewer.COMMAND.DRAW) {
+            Mockito.when(input.getKeyType()).thenReturn(KeyType.Character);
+            Mockito.when(input.getCharacter()).thenReturn('d');
+        }
+        else if (command == ArenaViewer.COMMAND.QUIT) {
+            Mockito.when(input.getKeyType()).thenReturn(KeyType.Character);
+            Mockito.when(input.getCharacter()).thenReturn('q');
+        }
+        else if (command == ArenaViewer.COMMAND.SWITCH) {
+            Mockito.when(input.getKeyType()).thenReturn(KeyType.Enter);
+        }
+        else if (command == ArenaViewer.COMMAND.PLAYCARD) {
+            Mockito.when(input.getKeyType()).thenReturn(KeyType.Tab);
+        }
+        else{
+            Mockito.when(input.getKeyType()).thenReturn(KeyType.Backspace);
+        }
 
-        Mockito.when(input.getKeyType()).thenReturn(KeyType.Character);
-        Mockito.when(input.getCharacter()).thenReturn('2');
-        assertEquals(ArenaViewer.COMMAND.TWO, viewer.getNextCommand());
-
-        Mockito.when(input.getKeyType()).thenReturn(KeyType.Character);
-        Mockito.when(input.getCharacter()).thenReturn('3');
-        assertEquals(ArenaViewer.COMMAND.THREE, viewer.getNextCommand());
-
-        Mockito.when(input.getKeyType()).thenReturn(KeyType.Character);
-        Mockito.when(input.getCharacter()).thenReturn('4');
-        assertEquals(ArenaViewer.COMMAND.FOUR, viewer.getNextCommand());
-
-        Mockito.when(input.getKeyType()).thenReturn(KeyType.Character);
-        Mockito.when(input.getCharacter()).thenReturn('d');
-        assertEquals(ArenaViewer.COMMAND.DRAW, viewer.getNextCommand());
-
-        Mockito.when(input.getKeyType()).thenReturn(KeyType.Character);
-        Mockito.when(input.getCharacter()).thenReturn('q');
-        assertEquals(ArenaViewer.COMMAND.QUIT, viewer.getNextCommand());
-
-        Mockito.when(input.getKeyType()).thenReturn(KeyType.Backspace);
-        assertEquals(ArenaViewer.COMMAND.NOTHING, viewer.getNextCommand());
-
-        Mockito.when(input.getKeyType()).thenReturn(KeyType.Enter);
-        assertEquals(ArenaViewer.COMMAND.SWITCH, viewer.getNextCommand());
-
-        Mockito.when(input.getKeyType()).thenReturn(KeyType.Tab);
-        assertEquals(ArenaViewer.COMMAND.PLAYCARD, viewer.getNextCommand());
+        assertEquals(command, viewer.getNextCommand());
     }
 }
--- src/test/java/com/g13/view/menus/MenuViewerTest.java
+++ src/test/java/com/g13/view/menus/MenuViewerTest.java
@@ -5,7 +5,8 @@ import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.TerminalScreen;
-import org.junit.jupiter.api.Test;
+import net.jqwik.api.ForAll;
+import net.jqwik.api.Property;
 import org.mockito.Mockito;
 import com.googlecode.lanterna.input.KeyStroke;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,8 +15,8 @@ import java.io.IOException;
 import static org.mockito.ArgumentMatchers.*;
 
 public class MenuViewerTest {
-    @Test
-    public void testKeyboardInput() throws IOException {
+    @Property
+    public void testKeyboardInput(@ForAll MenuViewer.COMMAND command) throws IOException {
         TerminalScreen screen = Mockito.mock(TerminalScreen.class);
         Level menu = Mockito.mock(Level.class);
         KeyStroke input = Mockito.mock(KeyStroke.class);
@@ -30,23 +31,20 @@ public class MenuViewerTest {
         LevelViewer viewer = new LevelViewer(menu, screen);
 
         Mockito.when(screen.readInput()).thenReturn(input);
-        Mockito.when(input.getKeyType()).thenReturn(KeyType.ArrowDown);
-        assertEquals(MenuViewer.COMMAND.DOWN, viewer.getNextCommand());
 
-        Mockito.when(input.getKeyType()).thenReturn(KeyType.ArrowUp);
-        assertEquals(MenuViewer.COMMAND.UP, viewer.getNextCommand());
+        if (command == MenuViewer.COMMAND.DOWN)
+            Mockito.when(input.getKeyType()).thenReturn(KeyType.ArrowDown);
+        else if (command == MenuViewer.COMMAND.UP)
+            Mockito.when(input.getKeyType()).thenReturn(KeyType.ArrowUp);
+        else if (command == MenuViewer.COMMAND.SELECT)
+            Mockito.when(input.getKeyType()).thenReturn(KeyType.Enter);
+        else if (command == MenuViewer.COMMAND.QUIT) {
+            Mockito.when(input.getKeyType()).thenReturn(KeyType.Character);
+            Mockito.when(input.getCharacter()).thenReturn('q');
+        }
+        else
+            Mockito.when(input.getKeyType()).thenReturn(KeyType.Backspace);
 
-        Mockito.when(input.getKeyType()).thenReturn(KeyType.Enter);
-        assertEquals(MenuViewer.COMMAND.SELECT, viewer.getNextCommand());
-
-        Mockito.when(input.getKeyType()).thenReturn(KeyType.ArrowDown);
-        assertEquals(MenuViewer.COMMAND.DOWN, viewer.getNextCommand());
-
-        Mockito.when(input.getKeyType()).thenReturn(KeyType.Character);
-        Mockito.when(input.getCharacter()).thenReturn('q');
-        assertEquals(MenuViewer.COMMAND.QUIT, viewer.getNextCommand());
-
-        Mockito.when(input.getKeyType()).thenReturn(KeyType.Backspace);
-        assertEquals(MenuViewer.COMMAND.NOTHING, viewer.getNextCommand());
+        assertEquals(command, viewer.getNextCommand());
     }
 }
